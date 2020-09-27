eca2d44e4337d0a19f894dbad5665d1d1cc4c269
Danny Lin <danny@kdrag0n.dev>
2020-09-26 17:32:51 -0700

    SystemUI: Hide section headers in notification shade
    Distinguishing different types of notifications (incoming, conversations,
    alterting, and silent) can be useful, but there's no need to use so much
    of the limited notification shade space for the section headers.
    
    Let's hide the section headers to save space while keeping the section
    cards and padding for some minor visual separation.
    
    Change-Id: Icecfe5ac29ec4c21ffdf6ce13812b8d1a66b0f69

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationSectionsManager.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationSectionsManager.kt
@@ -156,16 +156,20 @@ class NotificationSectionsManager @Inject internal constructor(
             setHeaderText(R.string.notification_section_header_gentle)
             setOnHeaderClickListener { onGentleHeaderClick() }
             setOnClearAllClickListener { onClearGentleNotifsClick(it) }
+            setVisibility(View.GONE)
         }
         alertingHeaderView = reinflateView(
                 alertingHeaderView, layoutInflater, R.layout.status_bar_notification_section_header
         ).apply {
             setHeaderText(R.string.notification_section_header_alerting)
             setOnHeaderClickListener { onGentleHeaderClick() }
+            setVisibility(View.GONE)
         }
         peopleHubSubscription?.unsubscribe()
         peopleHubSubscription = null
-        peopleHeaderView = reinflateView(peopleHeaderView, layoutInflater, R.layout.people_strip)
+        peopleHeaderView = reinflateView(peopleHeaderView, layoutInflater, R.layout.people_strip).apply {
+            setVisibility(View.GONE)
+        }
         if (ENABLE_SNOOZED_CONVERSATION_HUB) {
             peopleHubSubscription = peopleHubViewAdapter.bindView(peopleHubViewBoundary)
         }
@@ -174,6 +178,7 @@ class NotificationSectionsManager @Inject internal constructor(
         ).apply {
             setHeaderText(R.string.notification_section_header_incoming)
             setOnHeaderClickListener { onGentleHeaderClick() }
+            setVisibility(View.GONE)
         }
         mediaControlsView =
                 reinflateView(mediaControlsView, layoutInflater, R.layout.keyguard_media_header)

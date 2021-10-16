8da5059b4b2e5f3422e0d9c88885857074381917
Danny Lin <danny@kdrag0n.dev>
2021-10-16 05:28:33 -0700

    gmscompat: Add compatibility layer for unprivileged Google Play services
    This is a compatibility layer that allows installing Google Play
    services, Google Play Store, and Google Services Framework as regular,
    unprivileged apps in the standard app sandbox. Most functionality works
    without giving Google Play Services any special privileges, permissions,
    or security exceptions, including:
    
    - Account login (including two-factor authentication with NFC security keys)
    - Play Store
    - Firebase Cloud Messaging notifications (tested with Signal, Discord, Slack, and Gmail)
    - Firebase database API (tested with Swift Backup)
    - Firebase app indexing
    - Google Play Games
    - Account settings
    - Google My Account
    - Autofill
    - SMS verification receiver (tested with Signal)
    - Play license verification (both in-app purchases and paid apps)
    - Play Store app purchases
    - Play Store app installation (buggy)
    - SafetyNet (basic integrity passes, but not CTS profile checks)
    - Dynamite modules (e.g. Maps API v2 and Cronet)
    - Play geolocation API (buggy)
    
    The Google Play services family of apps can also be installed in a
    secondary user profile for isolation, and all of the above functionality
    will still work.
    
    NB: The DropBoxManager and DeviceConfig shims are not functionally
    meaningful, but they help reduce log spam that could obscure fatal
    errors.
    
    Change-Id: Ib6f6b36946dab2f6bb51650c1b6aabb628af46be

--- apex/jobscheduler/framework/java/android/app/AlarmManager.java
+++ apex/jobscheduler/framework/java/android/app/AlarmManager.java
@@ -26,6 +26,7 @@ import android.annotation.SdkConstant.SdkConstantType;
 import android.annotation.SystemApi;
 import android.annotation.SystemService;
 import android.annotation.TestApi;
+import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
 import android.compat.annotation.UnsupportedAppUsage;
@@ -43,6 +44,7 @@ import android.util.Log;
 import android.util.proto.ProtoOutputStream;
 
 import com.android.i18n.timezone.ZoneInfoDb;
+import com.android.internal.gmscompat.GmsHooks;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1172,6 +1174,11 @@ public class AlarmManager {
     @RequiresPermission(value = Manifest.permission.SCHEDULE_EXACT_ALARM, conditional = true)
     public void setExactAndAllowWhileIdle(@AlarmType int type, long triggerAtMillis,
             PendingIntent operation) {
+        if (GmsCompat.isEnabled() && !canScheduleExactAlarms()) {
+            setAndAllowWhileIdle(type, triggerAtMillis, operation);
+            return;
+        }
+
         setImpl(type, triggerAtMillis, WINDOW_EXACT, 0, FLAG_ALLOW_WHILE_IDLE, operation,
                 null, null, (Handler) null, null, null);
     }

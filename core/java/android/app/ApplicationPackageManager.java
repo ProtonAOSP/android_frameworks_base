b0b5380e88791f9c4bce94913d36931d10bd5304
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-04-04 18:10:19 +0300

    gmscompat: additional shims

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -220,7 +220,7 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public PackageInfo getPackageInfo(VersionedPackage versionedPackage, int flags)
             throws NameNotFoundException {
-        flags = GmsHooks.getPackageInfoFlags(flags);
+        flags = GmsHooks.filterPackageInfoFlags(flags);
 
         final int userId = getUserId();
         try {
@@ -238,7 +238,7 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public PackageInfo getPackageInfoAsUser(String packageName, int flags, int userId)
             throws NameNotFoundException {
-        flags = GmsHooks.getPackageInfoFlags(flags);
+        flags = GmsHooks.filterPackageInfoFlags(flags);
 
         PackageInfo pi =
                 getPackageInfoAsUserCached(
@@ -1111,6 +1111,8 @@ public class ApplicationPackageManager extends PackageManager {
     @SuppressWarnings("unchecked")
     @Override
     public List<PackageInfo> getInstalledPackages(int flags) {
+        flags = GmsHooks.filterPackageInfoFlags(flags);
+
         return getInstalledPackagesAsUser(flags, getUserId());
     }
 
@@ -2849,6 +2851,11 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public void setApplicationEnabledSetting(String packageName,
                                              int newState, int flags) {
+        if (GmsCompat.isPlayStore()) {
+            PlayStoreHooks.setApplicationEnabledSetting(packageName, newState);
+            return;
+        }
+
         try {
             mPM.setApplicationEnabledSetting(packageName, newState, flags,
                     getUserId(), mContext.getOpPackageName());

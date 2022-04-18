79dd748613a4f0c99464d5cda75d6f2c9edbeaa4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-04-18 15:08:14 +0300

    gmscompat: simplify initialization
    Change-Id: I2eee206609a242a64e5405795bf5107581553512

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -19,6 +19,7 @@ package android.app;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
@@ -56,9 +57,7 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-
 import com.android.internal.gmscompat.AttestationHooks;
-import com.android.internal.gmscompat.GmsHooks;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1188,7 +1187,7 @@ public class Instrumentation {
     public Application newApplication(ClassLoader cl, String className, Context context)
             throws InstantiationException, IllegalAccessException, 
             ClassNotFoundException {
-        GmsHooks.initApplicationBeforeOnCreate(context);
+        GmsCompat.maybeEnable(context);
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
@@ -1208,7 +1207,7 @@ public class Instrumentation {
     static public Application newApplication(Class<?> clazz, Context context)
             throws InstantiationException, IllegalAccessException, 
             ClassNotFoundException {
-        GmsHooks.initApplicationBeforeOnCreate(context);
+        GmsCompat.maybeEnable(context);
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         AttestationHooks.initApplicationBeforeOnCreate(app);

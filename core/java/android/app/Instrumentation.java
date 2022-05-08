66ae16e9139c192648c59f1d5645e44d6653fcf6
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-05-08 19:13:48 +0300

    gmscompat: fix false-negative "can GMS start an Activity" check
    hasAtLeastOneResumedActivity() isn't a reliable check for whether an app
    can start an Activity.
    ActivityManagerService has a much more complex set of checks that allow
    an app to start an Activity even if it doesn't have any visible
    activities in case the user interacted with it (by tapping
    a notification, quick tile etc) shortly before the activity start
    request was made. Unfortunately, activity start is silently aborted if
    this check fails, (no callback of any kind to the caller),
    requiring a patch to ActivityManagerService.
    
    Change-Id: Ic7a22660155fe421f9a4dfd3f0b28ac3d4039c01

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -58,6 +58,7 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 import com.android.internal.gmscompat.AttestationHooks;
+import com.android.internal.gmscompat.GmsHooks;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1766,6 +1767,11 @@ public class Instrumentation {
                     intent.resolveTypeIfNeeded(who.getContentResolver()), token,
                     target != null ? target.mEmbeddedID : null, requestCode, 0, null, options);
             checkStartActivityResult(result, intent);
+
+            if (GmsCompat.isEnabled()) {
+                GmsHooks.onActivityStart(result, intent, options);
+            }
+
         } catch (RemoteException e) {
             throw new RuntimeException("Failure from system", e);
         }
